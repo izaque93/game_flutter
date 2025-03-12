d6a2e7996a07082e4a4a3e1a23f37003679100f5
izaque <izaquemalta93@gmail.com>
2025-03-12 20:24:59 -0300

    added ground and other control blocks

---
+++ lib/ember_quest.dart
@@ -0,0 +1,66 @@
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flame_game/actors/water_enemy.dart';
+import 'package:flame_game/managers/segment_manager.dart';
+import 'package:flame_game/objects/ground_block.dart';
+import 'package:flame_game/objects/platform_block.dart';
+import 'package:flame_game/objects/star.dart';
+import 'package:flutter/material.dart';
+
+import 'actors/ember.dart';
+
+class EmberQuestGame extends FlameGame {
+  late EmberPlayer _ember;
+  double objectSpeed = 0.0;
+
+  @override
+  Future<void> onLoad() async {
+    await images.loadAll([
+      'block.png',
+      'ember.png',
+      'ground.png',
+      'heart_half.png',
+      'heart.png',
+      'star.png',
+      'water_enemy.png',
+    ]);
+
+    camera.viewfinder.anchor = Anchor.topLeft;
+    initializeGame();
+  }
+
+  void loadGamesSegments(int segmentIndex, double xPositionOffset) {
+    for (final block in segments[segmentIndex]) {
+      switch (block.blockType) {
+        case GroundBlock:
+        case PlatformBlock:
+          add(PlatformBlock(
+            gridPosition: block.gridPosition,
+            xOffset: xPositionOffset,
+          ));
+        case Star:
+        case WaterEnemy:
+      }
+    }
+  }
+
+  void initializeGame() {
+    // Assume that size.x < 3200
+    final segmentsToLoad = (size.x / 640).ceil();
+    segmentsToLoad.clamp(0, segments.length);
+
+    for (var i = 0; i <= segmentsToLoad; i++) {
+      loadGamesSegments(i, (640 * i).toDouble());
+    }
+
+    _ember = EmberPlayer(
+      position: Vector2(128, canvasSize.y - 70),
+    );
+    world.add(_ember);
+  }
+
+  @override
+  Color backgroundColor() {
+    return const Color.fromARGB(255, 173, 223, 247);
+  }
+}
